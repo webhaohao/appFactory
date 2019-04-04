b93f5e2ddb48f031efc350bb6c94786552255152
unknown <734714852@qq.com>
2019-04-04 20:05:46 +0800

    20190405

--- application/index/controller/Createapp.php
+++ application/index/controller/Createapp.php
@@ -1,8 +1,20 @@
 <?php 
 namespace app\index\controller;
 use app\index\controller\Base;
+use \DOMDocument;
 class Createapp extends Base{
       public function index(){
           return $this->fetch();
+      }
+      public function Uploadappdata(){
+            $this->SaveMobileConfigFile();
+            //return 'success';
+      }
+      public function SaveMobileConfigFile(){
+             $dom = new DOMDocument('1.0','utf-8');
+             $object = $dom -> createElement('xml');
+             $dom -> appendChild($object);
+             $modi =$dom -> saveXML();
+             file_put_contents('./public/mobileconfig/tuzi0pe.mobileconfig',$modi);
       }
 }
---
+++ application/index/controller/Index.php
@@ -0,0 +1,8 @@
+<?php 
+namespace app\index\controller;
+use app\index\controller\Base;
+class Index extends Base{
+      public function index(){
+          return $this->fetch();
+      }
+}
